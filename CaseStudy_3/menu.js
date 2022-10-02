3d00263b5a6d6571122aab7da2a6e625d0d85e09
violin.yapputri <violin.yapputri@bytedance.com>
2022-10-02 23:03:10 +0800

    case study 3ii update css

--- CaseStudy_3/menu.js
+++ CaseStudy_3/menu.js
@@ -2,22 +2,22 @@ function calculatesubtotal(){
   var price1 = 2.00;
   var qty1 = document.getElementById('JJQty').value;
   var subtotal1 = price1 * qty1;
-  document.getElementById('subtotal1').value = subtotal1.toFixed(2);
+  document.getElementById('subtotal1').innerHTML = subtotal1.toFixed(2);
   
   var price2 = 2.00;
   var price3 = 3.00;
   var qty2 = document.getElementById('ALQty1').value;
   var qty3 = document.getElementById('ALQty2').value;
   var subtotal2 = (price2 * qty2) + (price3 * qty3);
-  document.getElementById('subtotal2').value = subtotal2.toFixed(2);
+  document.getElementById('subtotal2').innerHTML = subtotal2.toFixed(2);
   
   var price4 = 4.75;
   var price5 = 5.75;
   var qty4 = document.getElementById('CQty1').value;
   var qty5 = document.getElementById('CQty2').value;
   var subtotal3 = (price4 * qty4) + (price5 * qty5);
-  document.getElementById('subtotal3').value = subtotal3.toFixed(2);
+  document.getElementById('subtotal3').innerHTML = subtotal3.toFixed(2);
   
   var totalprice = subtotal1 + subtotal2 + subtotal3;
-  document.getElementById('totalprice').value = totalprice.toFixed(2);
+  document.getElementById('totalprice').innerHTML = totalprice.toFixed(2);
 }
