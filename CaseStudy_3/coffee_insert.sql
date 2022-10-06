d9795f65733a3c3fd71cdb716c71daa4532afeff
violin.yapputri <violin.yapputri@bytedance.com>
2022-10-06 13:58:45 +0800

    add database

---
+++ CaseStudy_3/coffee_insert.sql
@@ -0,0 +1,8 @@
+use myuser;
+
+insert into coffee values
+  (NULL, "Just Java", "Endless Cup", 2.00),
+  (NULL, "Cafe au Lait", "Single", 2.00),
+  (NULL, "Cafe au Lait", "Double", 3.00),
+  (NULL, "Iced Cappuccino", "Single", 4.75),
+  (NULL, "Iced Cappuccino", "Double", 5.75);
---
+++ CaseStudy_3/javajam.sql
@@ -0,0 +1,6 @@
+create table coffee
+( coffeeid int unsigned not null auto_increment primary key,
+  name char(50) not null,
+  type char(50) not null,
+  price float unsigned,
+)
