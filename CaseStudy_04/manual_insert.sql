bfbeb207f7bc1fa186a64efaa71a2b11f10c9a10
jslim0397 <114211263+jslim0397@users.noreply.github.com>
2022-10-19 16:33:16 +0800

    Update manual_insert.sql

--- CaseStudy_04/manual_insert.sql
+++ CaseStudy_04/manual_insert.sql
@@ -2,7 +2,4 @@ use orderlist;
 
 insert into orders values
   (1, "1", "null","1", "Single", "1", "Single", "8.75"),
-  (2, "1", "null","1", "Double", "1", "Single", "9.75"),
-  (3, "1", "null","1", "Single", "1", "Double", "9.75"),
-  (4, "1", "null","1", "Double", "1", "Double", "10.75"),
-  (5, "1", "null","2", "Double", "2", "Double", "21.50");
+  (2, "1", "null","1", "Double", "1", "Double", "8.75"),
