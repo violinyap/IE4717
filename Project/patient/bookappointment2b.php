18e3146f6c71b851dbf9c458b186b23a4a00d991
jslim0397 <114211263+jslim0397@users.noreply.github.com>
2022-10-31 19:09:09 +0800

    Added previous function

--- Project/patient/bookappointment2b.php
+++ Project/patient/bookappointment2b.php
@@ -27,6 +27,30 @@
 		$doctor = $_POST['doctor'];
 		$location = $_POST['location'];
 		$date = $_POST['date'];
+		if (!empty($_POST['location'])) {
+			$location = $_POST['location'];
+		}
+		else {
+			$location = "";
+		}
+		if (!empty($_POST['doctor'])) {
+			$doctor = $_POST['doctor'];
+		}
+		else {
+			$doctor = "";
+		}
+		if (!empty($_POST['date'])) {
+			$date = $_POST['date'];
+		}
+		else {
+			$date = "";
+		}
+		if (!empty($_POST['timeslot'])) {
+			$time2 = $_POST['timeslot'];
+		}
+		else {
+			$time2 = "";
+		}
 	?>
 		<form method="post" action="bookappointment3.php" id="appointmentform">
 			<?php
@@ -43,6 +67,7 @@
 			$result = mysqli_query($db,$sql);
 			$items = array();
 			$items2 = array((strtotime("08:00")),(strtotime("09:00")), (strtotime("10:00")), (strtotime("11:00")),(strtotime("12:00")),(strtotime("13:00")),(strtotime("14:00")),(strtotime("15:00")), (strtotime("16:00")), (strtotime("17:00")));
+			//$time2 = array("08:00");
 			$firstTime = strtotime("08:00");
 			$lastTime = strtotime("17:00");
 			while ($row = mysqli_fetch_assoc($result)){
@@ -59,40 +84,75 @@
 			}
 			$items3 = array();
 			$items3 = array_diff($items2,$items);
+			$time3 = array();
+			$time3 = date("H:i" ,reset($items3));
 			foreach($items3 as $items3){
 			$time = date("H:i" , $items3);
+			if ($time == $time2) {
 			echo "
 			<tr class='time-row'>
-			<td>
-			<p><input type='radio' value='". $time ."' name='timeslot' onInput='getTime();' checked='checked'>
+			<td colspan='2'>
+			<p><input type='radio' value='". $time ."' name='timeslot' onInput='getTime();' checked>
 			<label>" . $time . "</label><br></p>
 			</td>
 			</tr>
 			";
 			}
+			else if ($time == $time3) {
+			echo "
+			<tr class='time-row'>
+			<td colspan='2'>
+			<p><input type='radio' value='". $time ."' name='timeslot' onInput='getTime();' checked>
+			<label>" . $time . "</label><br></p>
+			</td>
+			</tr>
+			";
+			}
+			else {
+			echo "
+			<tr class='time-row'>
+			<td colspan='2'>
+			<p><input type='radio' value='". $time ."' name='timeslot' onInput='getTime();' required>
+			<label>" . $time . "</label><br></p>
+			</td>
+			</tr>
+			";
+			}
+			}
 			if (empty($items3)) {
 			echo "
-			<tr><td>All slots occupied! Press 'Back' to restart.</td></tr>
-			<tr style='height:540px;'><td></td></tr>
+			<tr><td colspan='2'>All slots occupied! Go previous pages to change selection.
+			<br>Or press 'restart to start from Step 1a again.
+			<br>Please check <a href='doctors.php' id='botnav'>'Our Doctors'</a> page for their available schedule.
+			</td></tr>
+			<tr style='height:540px;'><td colspan='2'></td></tr>
 			<tr><td>
-			<a href='bookappointment1a.php' id='cancelButton'>Back</a>
+			<input type='hidden' name='date' value='" .$date. "'></input>
+			<input type='hidden' name='location' value='" .$location. "'></input>
+			<input type='hidden' name='doctor' value='" .$doctor. "'></input>
+			<input type='submit' value='Previous' formaction='bookappointment2b.php' id='nextBtn'></input>
+			</td></tr>
+			<td>
+			<input type='submit' value='Restart' formaction='bookappointment1a.php' id='nextBtn'></input>
 			</td></tr>
 			";
 			}
 			else {
 			echo "
-			<tr style='height:25px;'><td></td></tr>
+			<tr style='height:25px;'><td colspan='2'></td></tr>
 			<tr><td>
 			<input type='hidden' name='date' value='" .$date. "'></input>
 			<input type='hidden' name='location' value='" .$location. "'></input>
 			<input type='hidden' name='doctor' value='" .$doctor. "'></input>
+			<input type='submit' value='Previous' formaction='bookappointment2a.php' id='nextBtn'></input>
+			</td>
+			<td>
 			<input type='submit' value='Next' id='nextBtn'></input>
-			</tr></td>
+			</td></tr>
 			";
 			}
 			$result->free();
 			$db->close();
-			
 			echo "</table>";
 			?>
 		</form>
