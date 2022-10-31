46ebab967337a4fd94f52f636d353b358c3ccdf9
jslim0397 <114211263+jslim0397@users.noreply.github.com>
2022-10-31 19:07:14 +0800

    Added 'Previous' function

--- Project/patient/bookappointment1a.php
+++ Project/patient/bookappointment1a.php
@@ -23,38 +23,77 @@
 		<form method="post" action="bookappointment1b.php" id="appointmentform">
 			<table>
 			<th style="float:left;">Step 1a:</th>
-			<tr><td>Select our locations.</td></tr>
+			<tr><td colspan='2'>Select our locations.</td></tr>
 			<tr style="height:25px;"><td></td></tr>
-			<tr><td>View <a href="doctors.php" id="botnav"><i>'Our Doctors'</i></a> for their location, and available date and time slots.</td></tr>
+			<tr><td colspan='2'>View <a href="doctors.php" id="botnav"><i>'Our Doctors'</i></a> for their location, and available date and time slots.</td></tr>
 			<tr style="height:25px;"><td></td></tr>
-			<tr><td>Locations: </td></tr>
-			<tr style="height:25px;"><td></td></tr>
+			<tr><td colspan='2'>Locations: </td></tr>
+			<tr style="height:25px;"><td colspan='2'></td></tr>
 			<?php 
-							include "../methods/dbconnect.php";
-							$query = "SELECT * FROM clinics";
-							// echo "<br>" .$query. "<br>";
-							$result = $dbcnx->query($query);
-							echo "<input value='$apptID' name='apptID' type='hidden'/>";
-							if ($result->num_rows >0 )
-							{
-								while ($row = mysqli_fetch_assoc($result)){
-									$clinic_name = $row['clinicname'];
-									$clinic_id = $row['clinicid'];
-									echo '
-									<tr><td><input type="radio" value="'.$clinic_id.'" id="clinic_'.$row['clinicid'].'" name="location" checked> 
-									<label for="">'.$clinic_name.'</label>
-									</td></tr>
-									';
-									
-									
-									
-								}
-							}
-							$dbcnx->close();
-					?>
-			<tr style="height:400px;"><td></td></tr>
-			<tr><td><input type="submit" value="Next" id="nextBtn"></input>
-			</td></tr>
+				include "../methods/dbconnect.php";
+				
+				if (!empty($_POST['location'])) {
+					$location = $_POST['location'];
+				}
+				else {
+					$location = "1";
+				}
+				if (!empty($_POST['doctor'])) {
+					$doctor = $_POST['doctor'];
+				}
+				else {
+					$doctor = "";
+				}
+				if (!empty($_POST['date'])) {
+					$date = $_POST['date'];
+				}
+				else {
+					$date = "";
+				}
+				if (!empty($_POST['timeslot'])) {
+					$time = $_POST['timeslot'];
+				}
+				else {
+					$time = "";
+				}
+				$query = "SELECT * FROM clinics";
+				// echo "<br>" .$query. "<br>";
+				$result = $dbcnx->query($query);
+				//echo "<input value='$apptID' name='apptID' type='hidden'/>";
+				if ($result->num_rows >0 )
+				{
+					while ($row = mysqli_fetch_assoc($result)){
+					$clinic_name = $row['clinicname'];
+					$clinic_id = $row['clinicid'];
+						if ($location == $clinic_id) {
+							echo '
+							<tr><td><input type="radio" value="'.$clinic_id.'" id="clinic_'.$row['clinicid'].'" name="location" checked>
+							';
+						}
+						else {
+						echo '
+						<tr><td><input type="radio" value="'.$clinic_id.'" id="clinic_'.$row['clinicid'].'" name="location" required>
+						';
+						}
+					echo '
+					<label for="">'.$clinic_name.'</label>
+					</td></tr>
+					<tr style="height:10px;"><td colspan="2"></td></tr>
+					';
+					}
+				}
+				$dbcnx->close();
+			?>
+			<tr style="height:450px;"><td colspan='2'></td></tr>
+			<tr>
+			<td>
+			<input type="submit" value="Previous" formaction="bookappointment.php" id="nextBtn"></input></td>
+			<td>
+			<input type="hidden" name="date" value='<?php echo "$date";?>'></input>
+			<input type="hidden" name="doctor" value='<?php echo "$doctor"; ?>'></input>
+			<input type="hidden" name="timeslot" value='<?php echo "$time"; ?>'></input>
+			<input type="submit" value="Next" id="nextBtn"></input></td>
+			</tr>
 			</table>
 		</form>
 	</div>
