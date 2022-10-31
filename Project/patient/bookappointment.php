a02f8170594a3747b7fa810ebd1e366a86ec9d34
jslim0397 <114211263+jslim0397@users.noreply.github.com>
2022-11-01 05:25:33 +0800

    Added show options

--- Project/patient/bookappointment.php
+++ Project/patient/bookappointment.php
@@ -34,6 +34,12 @@
 			else {
 				$doctor = "";
 			}
+			if (!empty($_POST['doctor2'])) {
+				$doctor2 = $_POST['doctor2'];
+			}
+			else {
+				$doctor2 = "";
+			}
 			if (!empty($_POST['date'])) {
 				$date = $_POST['date'];
 			}
@@ -47,6 +53,7 @@
 				$time = "";
 			}
 	?>
+	<div style="float:left; width: 800px;">
 	<form method="post" action="bookappointment1a.php" id="appointmentform">
 	<table>
 		<th style="float:left;font-size:24px;">Start booking an apppointment?</th>
@@ -61,11 +68,60 @@
 			<input type="hidden" name="date" value='<?php echo "$date";?>'></input>
 			<input type="hidden" name="location" value='<?php echo "$location"; ?>'></input>
 			<input type="hidden" name="doctor" value='<?php echo "$doctor"; ?>'></input>
+			<input type="hidden" name="doctor2" value='<?php echo "$doctor2"; ?>'></input>
 			<input type="hidden" name="timeslot" value='<?php echo "$time"; ?>'></input>
 			<input type="submit" value="Next" id="nextBtn"></input>
 		</td></tr>
 	</table>
 	</form>
+	</div>
+		<div style="float:right; background-color: white; padding-top: 20px; width: 350px;">
+		<table style="border:1px solid black;">
+		<th>Options Selected:</th>
+		<tr><td colspan="2" style="height:25px;"></td></tr>
+		<tr><th style="text-align:right;"> Location: </th>
+		<td style="width:200px;background-color: #D9D9D9; border: 1px solid black;text-align:center;">
+		<?php
+			if ($location == "") {
+			echo "TBD";}
+			else {
+			if ($location == 1) {
+			echo "NTU Clinic Fullerton";}
+			else {echo "NTU Clinic Raffles";}
+			}
+		?>
+		</td></tr>
+		<tr><td colspan="2" style="height:25px;"></td></tr>
+		<tr><th style="text-align:right;"> Doctor: </th>
+		<td style="width:200px; background-color: #D9D9D9; border: 1px solid black;text-align:center;"> 
+		<?php
+			if ($doctor == "") {
+			echo "TBD";}
+			else {echo $doctor2;}
+		?>
+		</td></tr>
+		<tr><td colspan="2" style="height:25px;"></td></tr>
+		<tr><th style="text-align:right;"> Date: </th>
+		<td style="width:200px; background-color: #D9D9D9; border: 1px solid black;text-align:center;"> 
+		<span id="date_show">
+		<?php
+			if ($date == "") {
+			echo "TBD";}
+			else {echo $date;}
+		?>
+		</span>
+		</td></tr>
+		<tr><td colspan="2" style="height:25px;"></td></tr>
+		<tr><th style="text-align:right;"> Time: </th>
+		<td style="width:200px; background-color: #D9D9D9; border: 1px solid black;text-align:center;"> 
+		<?php
+			if ($time == "") {
+			echo "TBD";}
+			else {echo $time;}
+		?>
+		</td></tr>
+		</table>
+		</div>
 	</div>
 	</div>
 	<?php include "footer.php";?>
