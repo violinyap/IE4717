9bafa6d333b00486aceedfb8414c7f868c23a4fc
jslim0397 <jslim0397@gmail.com>
2022-11-02 21:45:50 +0800

    Update bookaptbox.php

--- Project/patient/bookaptbox.php
+++ Project/patient/bookaptbox.php
@@ -31,10 +31,20 @@
 					<tr class="selectedrow">
 						<td><b>Doctor:</b></td>
 						<td class="selectedoption">';
-            
-								if ($doctor2 == "") {
-								echo "TBD";}
-								else {echo $doctor2;}
+						if ($doctor == "") {
+							echo "TBD";}
+						else {
+							$query = "SELECT * FROM doctors WHERE doctorid='".$doctor."'";
+							// echo "<br>" .$query. "<br>";
+							$result = $dbcnx->query($query);
+							if ($result->num_rows >0 )
+							{
+								while ($row = mysqli_fetch_assoc($result)){
+									$doctor_name = $row['docname'];
+									echo $doctor_name;
+								}
+							}
+						}
     echo '				
 						</td>
 					</tr>
