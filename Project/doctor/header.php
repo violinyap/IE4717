943623f761077897b5c3eed8c9d9e6c0d98beb65
violin.yapputri <violin.yapputri@bytedance.com>
2022-11-02 21:10:13 +0800

    doc logout

--- Project/doctor/header.php
+++ Project/doctor/header.php
@@ -20,10 +20,13 @@
   } 
   if (isset($_SESSION['valid_user']))
   { 
-    echo "<a href='doctor/profile.php' id='headerprofile'>";
-    echo "<img src='../images/profile.png' width='38' height='38' class='icon'>";
+    $usertype = $_SESSION['valid_user_type'];
+    $newpath = 'patient/profile.php';
+    if ($usertype == 'doctors') {$newpath = 'doctor/profile.php';}
+    echo "<a href='$newpath' id='headerprofile'>";
+    echo "<img src='images/profile.png' width='38' height='38' class='icon'>";
     echo $_SESSION["valid_user"]; 
-    echo "<form method=\"post\" action=\"logout.php\" ><button class=\"profiledrop\" type=\"submit\">V</button></form>";
+    echo "<form method=\"post\" action=\"login.php\" ><button class=\"profiledrop\" type=\"submit\">V</button></form>";
     echo "</a>";
   } 
   else { 
